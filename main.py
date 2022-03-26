532240c4f73cdc0ad1d8ec3478e124bba3e0a0de
Tamara Herrington <tamaraherrington24@gmail.com>
2022-03-26 22:10:39 +0000

    "set up git repo"

---
+++ main.py
@@ -0,0 +1,6 @@
+#welcome
+print("Congratulations new citizen! Welcome to the flagship Mars colony")
+
+# option for user to press enter to move to next statment
+
+user_planet = str(input("What planet are you from..."))
