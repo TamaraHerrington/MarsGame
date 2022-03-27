a2a12996e495b5a9f747a91192a1ae01af4b72cc
Tamara Herrington <tamaraherrington24@gmail.com>
2022-03-27 11:06:54 +0100

    add comment

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ print("Congratulations new citizen! Welcome to the flagship Mars colony")
 # option for user to press enter to move to next statment
 
 user_planet = str(input("What planet are you from..."))
+
+#take input from user
+#if planet == Mars: throw error
